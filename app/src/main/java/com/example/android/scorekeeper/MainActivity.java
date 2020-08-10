df262a2e8f06ce19d0115b9ccf6a1ed4d1b72913
jy-hui <huijy-wm19@student.tarc.edu.my>
2020-08-11 00:59:42 +0800

    Commit by Hui Jun Yoong

---
+++ app/src/main/java/com/example/android/scorekeeper/MainActivity.java
@@ -0,0 +1,128 @@
+/*
+ * Copyright (C) 2018 Google Inc.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.example.android.scorekeeper;
+
+import android.content.SharedPreferences;
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.app.AppCompatDelegate;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.TextView;
+
+
+public class MainActivity extends AppCompatActivity {
+
+    private int mScore1;
+    private int mScore2;
+    private TextView mScoreText1;
+    private TextView mScoreText2;
+    static final String STATE_SCORE_1 = "Team 1 Score";
+    static final String STATE_SCORE_2 = "Team 2 Score";
+    // Homework Part
+    SharedPreferences sharedPreferences;
+    private static final String sharedPrefFile = "com.example.scorekeeper";
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        mScoreText1 = findViewById(R.id.score_1);
+        mScoreText2 = findViewById(R.id.score_2);
+
+        // Homework Part
+        sharedPreferences = getSharedPreferences(sharedPrefFile, MODE_PRIVATE);
+        mScore1 = sharedPreferences.getInt(STATE_SCORE_1, 0);
+        mScore2 = sharedPreferences.getInt(STATE_SCORE_2, 0);
+        mScoreText1.setText(String.valueOf(mScore1));
+        mScoreText2.setText(String.valueOf(mScore2));
+    }
+
+    public void decreaseScore(View view) {
+        int viewID = view.getId();
+        switch (viewID) {
+            case R.id.decreaseTeam1:
+                mScore1--;
+                mScoreText1.setText(String.valueOf(mScore1));
+                break;
+            case R.id.decreaseTeam2:
+                mScore2--;
+                mScoreText2.setText(String.valueOf(mScore2));
+        }
+    }
+
+    public void increaseScore(View view) {
+        int viewID = view.getId();
+        switch (viewID) {
+            case R.id.increaseTeam1:
+                mScore1++;
+                mScoreText1.setText(String.valueOf(mScore1));
+                break;
+            case R.id.increaseTeam2:
+                mScore2++;
+                mScoreText2.setText(String.valueOf(mScore2));
+        }
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.main_menu, menu);
+        int nightMode = AppCompatDelegate.getDefaultNightMode();
+        if (nightMode == AppCompatDelegate.MODE_NIGHT_YES) {
+            menu.findItem(R.id.night_mode).setTitle(R.string.day_mode);
+        } else {
+            menu.findItem(R.id.night_mode).setTitle(R.string.night_mode);
+        }
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        if (item.getItemId() == R.id.night_mode) {
+            int nightMode = AppCompatDelegate.getDefaultNightMode();
+            if (nightMode == AppCompatDelegate.MODE_NIGHT_YES) {
+                AppCompatDelegate.setDefaultNightMode
+                        (AppCompatDelegate.MODE_NIGHT_NO);
+            } else {
+                AppCompatDelegate.setDefaultNightMode
+                        (AppCompatDelegate.MODE_NIGHT_YES);
+            }
+            recreate();
+        }
+        return true;
+    }
+
+    // Homework part
+    @Override
+    protected void onPause() {
+        SharedPreferences.Editor editor = sharedPreferences.edit();
+        editor.putInt(STATE_SCORE_1, mScore1);
+        editor.putInt(STATE_SCORE_2, mScore2);
+        editor.apply();
+        super.onPause();
+    }
+
+    public void onResetClick(View view) {
+        SharedPreferences.Editor editor = sharedPreferences.edit();
+        editor.clear().apply();
+        mScore1 = 0;
+        mScore2 = 0;
+        mScoreText1.setText(String.valueOf(mScore1));
+        mScoreText2.setText(String.valueOf(mScore2));
+    }
+}
